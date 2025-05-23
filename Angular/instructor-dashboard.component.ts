9307ac68462c5b7e5e6dac5d8e242270d4c8d1b6
Shaman <76400524+ShamanS1@users.noreply.github.com>
2025-05-23 10:13:59 +0530

    Create instructor-dashboard.component.ts

---
+++ Angular/instructor-dashboard.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-instructor-dashboard',
+  templateUrl: './instructor-dashboard.component.html',
+  styleUrls: ['./instructor-dashboard.component.css']
+})
+export class InstructorDashboardComponent {}
