e8e63493030f0a97bd6d0adbe9ebfab7dc7de324
Shaman <76400524+ShamanS1@users.noreply.github.com>
2025-05-23 10:04:22 +0530

    Create HomeController.java

---
+++ SpringSecurity/HomeController.java
@@ -0,0 +1,18 @@
+package com.wecp.w3day5task1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "Welcome Admin";
+    }
+}
