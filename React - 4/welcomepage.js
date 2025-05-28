e6e71ead32458190d3619400f948ee64f672a456
Shaman <76400524+ShamanS1@users.noreply.github.com>
2025-05-28 09:42:21 +0530

    Create welcomepage.js

---
+++ React - 4/welcomepage.js
@@ -0,0 +1,13 @@
+WELCOME.JS
+
+import React from 'react';
+
+const Welcome = () => {
+  return(
+    <div>
+        <h1>WelcomePage</h1>
+        <p>welcome to homepage</p>
+    </div>
+  );
+};
+export default Welcome;
