42c13727292b3bc4236fe6332a7c5ccd0ae40046
Shaman <76400524+ShamanS1@users.noreply.github.com>
2025-05-28 09:41:39 +0530

    Create errorpage.js

---
+++ React - 4/errorpage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Error = () => {
+    return(
+        <div>
+            <h1>ErrorPage</h1>
+            <p>There are error just check.</p>
+        </div>
+    );
+};
+
+export default Error;
