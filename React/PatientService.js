1bd816e97d035067966676401c50671876b67dd4
Shaman <76400524+ShamanS1@users.noreply.github.com>
2025-07-10 18:09:42 +0530

    Update PatientService.js

--- React/PatientService.js
+++ React/PatientService.js
@@ -1,9 +1,22 @@
-let mockPatients = [];
 export const addPatient = async (patient) => {
-  const id = `P${(mockPatients.length + 1).toString().padStart(3, '0')}`;
-  mockPatients.push({ ...patient, patientID: id });
+ return fetch("url",{
+   method:'POST',
+   body:JSON.Stringfy(patient)
+ }).then((res)=>res.json)
 };
 
 export const getPatients = async () => {
-  return [...mockPatients];
+  return fetch("url").then((res)=>res.json);
 };
+
+
+
+// let mockPatients = [];
+// export const addPatient = async (patient) => {
+//   const id = `P${(mockPatients.length + 1).toString().padStart(3, '0')}`;
+//   mockPatients.push({ ...patient, patientID: id });
+// };
+
+// export const getPatients = async () => {
+//   return [...mockPatients];
+// };
